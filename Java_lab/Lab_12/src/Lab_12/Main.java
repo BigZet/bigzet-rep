25a5aacea5a788bb1d2872647c2c9a32309072dd
BigZetRep <bigzet200120@mail.ru>
2020-10-14 21:16:56 +0300

    Lab_12 upd

--- Java_lab/Lab_12/src/Lab_12/Main.java
+++ Java_lab/Lab_12/src/Lab_12/Main.java
@@ -1,10 +1,25 @@
 package Lab_12;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+
+
 public class Main {
+    public static void check_pswd(){
+        Pattern pattern = Pattern.compile("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(_)?.{8,})");
+        Scanner sc =new Scanner(System.in);
+        String text= sc.nextLine();
+        Matcher matcher = pattern.matcher(text);
+        System.out.println(matcher.find()?"Пароль соответсвует":"Пароль не соответсвует");
+    }
     public static void main(String[] args) {
-        TestRegEx testRegEx = new TestRegEx();
+        String[] regexprs = {"\\d*[\\.,]\\d{2}\\s(USD|RUB|EU)",  "([0-2][0-9]|3[0-1])\\/(0[0-9]|1[0-2])\\/(19|[2-9]\\d)\\d{2}", "[ \\t\\v\\r\\n\\f][0-9a-z]+@[0-9a-z.]+[\\n\\s]"};
+        TestRegEx testRegEx1 = new TestRegEx(regexprs[0]);
+        TestRegEx testRegEx2 = new TestRegEx(regexprs[1]);
+        TestRegEx testRegEx3 = new TestRegEx(regexprs[2]);
+        check_pswd();
     }
 }
--- Java_lab/Lab_12/src/Lab_12/TestRegEx.java
+++ Java_lab/Lab_12/src/Lab_12/TestRegEx.java
@@ -3,13 +3,14 @@ package Lab_12;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class TestRegEx {
-    String RegEx="[ \\t\\v\\r\\n\\f][0-9a-z]+@[0-9a-z.]+[\\n\\s]";
-    String Text="Пример использования регулярных выражений \n" +
-            "Дана строка: bigzet20012@mail.ru myhost@@@com.ru, @my.ru, Julia String @@@ma@iill@.r.u\n" +
+    String Text="\nПример использования регулярных выражений \n" +
+            "Дана строка: (1+8)-9 25.43 USD 666.666 RUB 89.89 EU 12/20/2001 04/09/1999 \n1/03/2001 bigzet200120@mail.ru myhost@@@com.ru, @my.ru, Julia String @@@ma@iill@.r.u\n" +
             "Регулярным выражением было найдено следующее совпадение:";
-    Pattern pattern = Pattern.compile(RegEx);
-    Matcher matcher = pattern.matcher(Text);
-    public TestRegEx(){
+
+
+    public TestRegEx(String RegEx){
+        Pattern pattern = Pattern.compile(RegEx);
+        Matcher matcher = pattern.matcher(Text);
         System.out.println(Text);
         while(matcher.find()){
             System.out.println(matcher.group().strip());
